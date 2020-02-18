7a3b0b6b1451f8aeb5faeb86af34aea870065bfb
Seremba <pserembae@gmail.com>
2020-02-18 23:45:13 +0300

    added the middleware to protect the app from bad words and frequent requests

--- server/index.js
+++ server/index.js
@@ -1,27 +1,56 @@
 const express = require('express');
 const cors = require('cors');
+const monk = require('monk');
+const Filter = require('bad-words');
+const rateLimit = require("express-rate-limit");
+
+
 const app = express();
+const db = monk('localhost/meower');
+const mews = db.get('mews');
+const filter = new Filter;
 
 app.use(cors());
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.json({
       message: 'Meower! ihiihhi'
   });
 });
+
+app.get("/mews", (req, res) => {
+    mews
+       .find()
+       .then(mews => {
+           res.json(mews);
+       });
+});
+
 function isValidMew(mew){
     return mew.name && mew.name.toString().trim() !== '' &&
     mew.content && mew.content.toString().trim() !== '';
 }
+
+app.use(rateLimit({
+    windowMs: 30* 1000, 
+    max: 1 
+  }));
+
 app.post('/mews', (req, res) => {
   if(isValidMew(req.body)){
       //insert into the DB
       const mew = {
-          name: req.body.name.toString(),
-          content: req.body.content.toString()
+          name: filter.clean(req.body.name.toString()),
+          content: filter.clean(req.body.content.toString()),
+          created: new Date()
       };
-      console.log(mew);
+      mews
+      .insert(mew)
+      .then(createdMew => {
+          res.json(createdMew);
+      })
   } else {
       res.status(422);
       res.json({
@@ -29,6 +58,8 @@ app.post('/mews', (req, res) => {
       });
   }
 });
+
+
 
 app.listen(5000, () => {
     console.log('Listening on http://localhost:5000');
